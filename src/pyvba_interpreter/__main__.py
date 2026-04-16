7f0fc534a6c512e2530137f0a7522e8200db6cb4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-15 21:01:07 -0400

    Update __main__.py

--- src/pyvba_interpreter/__main__.py
+++ src/pyvba_interpreter/__main__.py
@@ -3,7 +3,7 @@ from antlr4 import CommonTokenStream, FileStream
 from antlr4_vba.vbaLexer import vbaLexer as Lexer
 from antlr4_vba.vbaParser import vbaParser as Parser
 from pathlib import Path
-from pyvba_interpreter.vba_visitor import VbaVisitor
+from .vba_visitor import VbaVisitor
 
 
 def main() -> None:
