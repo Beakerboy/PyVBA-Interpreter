90841f01ad2ba25052af5c75d24c257692c7f976
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-16 10:13:03 -0400

    Update test_interpreter.py

--- tests/Functional/test_interpreter.py
+++ tests/Functional/test_interpreter.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from antlr4 import CommonTokenStream, FileStream
 from antlr4_vba.vbaLexer import vbaLexer as Lexer
@@ -24,9 +25,14 @@ def test_interpreter(mock_print) -> None:
 
 @patch('builtins.print')
 def test_msgbox(mock_print) -> None:
-    test_call = 'MsgBox "Hello World"'
-    expected = "Hello World"
+    test_call = 'MsgBox 1'
+    expected = "1"
     file_path = 'tests/files/test.bas'
+    try:
+        os.remove(file_path)
+    except FileNotFoundError:
+        # File did not exist; ignore the error
+        pass
     with open(file_path, "w", newline='\r\n') as file:
         file.write('Attribute VB_NAME = "HelloWorld"\n')
         file.write('Function hello()\n')
