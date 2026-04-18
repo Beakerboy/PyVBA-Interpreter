8a1da36f03189185d626aad77cc56295f0828330
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-18 14:48:28 -0400

    Update symbol_table.py

--- src/pyvba_interpreter/symbol_table.py
+++ src/pyvba_interpreter/symbol_table.py
@@ -7,4 +7,4 @@ T = TypeVar('T', bound='SymbolTable')
 class SymbolTable:
     def __init__(self: T) -> None:
         # Maps name -> the actual ParseTree node for that sub/function
-        self.definitions = {}
+        self.definitions: dict[str, 'ParserRuleContext'] = {}
