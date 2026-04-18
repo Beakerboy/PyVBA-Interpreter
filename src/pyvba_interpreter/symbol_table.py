d572b0f2f7608f750948c131a616bfc8ae5b508e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-18 14:44:43 -0400

    Update symbol_table.py

--- src/pyvba_interpreter/symbol_table.py
+++ src/pyvba_interpreter/symbol_table.py
@@ -1,4 +1,10 @@
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='SymbolTable')
+
+
 class SymbolTable:
-    def __init__(self):
+    def __init__(self: T) -> None:
         # Maps name -> the actual ParseTree node for that sub/function
         self.definitions = {}
