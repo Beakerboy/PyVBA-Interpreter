1d3c52860f753e41d0ee5fab8c8bcbe4bc621277
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-19 22:12:45 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -49,6 +49,7 @@ class VbaVisitor(Visitor):
             ctx: Parser.ExpressionContext) -> Any:
         number = ctx.getAltNumber()
         if number in [5, 7, 8, 9, 10, 11, 13]:
+            return number
             left = self.visit(ctx.expression(0))
             right = self.visit(ctx.expression(1))
             if isinstance(ctx.getChild(1), Parser.WscContext):
