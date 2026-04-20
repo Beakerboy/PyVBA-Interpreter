7e39ab8e7b8d3770494cb7709d0c05566d0db5af
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-20 10:32:40 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -114,24 +114,13 @@ class VbaVisitor(Visitor):
             i = 2
         op = ctx.getChild(i).symbol.text.upper()
         if op == "AND":
-            t1 = type(left)
-            t2 = type(right)
-            return f"{t1} and {t2}"
+            return left and right
         elif op == "OR":
-            t1 = type(left)
-            t2 = type(right)
-            return f"{t1} or {t2}"
+            return left or right
         elif op == "XOR":
-            t1 = type(left)
-            t2 = type(right)
-            return f"{t1} != {t2}"
+            return left != right
         elif op == "IMP":
-            t1 = type(left)
-            t2 = type(right)
-            return f"not {t1} or {t2}"
-        elif op == "EQV":
-            t1 = type(left)
-            t2 = type(right)
-            return f"{t1} == {t2}"
-        else:
-            raise Exception(f"Unknown Boolean Expression: {op}")
+            return not left or right
+        else:  # op == "EQV":
+            return left == right
+
