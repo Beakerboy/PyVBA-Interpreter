43bd05bf3af98cbddbd6fc612999b88e424d5f02
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-20 11:35:30 -0400

    Update test_interpreter.py

--- tests/Functional/test_interpreter.py
+++ tests/Functional/test_interpreter.py
@@ -6,6 +6,7 @@ from antlr4_vba.vbaParser import vbaParser as Parser
 from pyvba_interpreter.symbol_table import SymbolTable
 from pyvba_interpreter.vba_listener import VbaListener
 from pyvba_interpreter.vba_visitor import VbaVisitor
+from pyvba_interpreter.Exceptions.vba_compile_exception import VbaCompileException
 from typing import Any
 from unittest.mock import patch
 
@@ -78,3 +79,31 @@ def test_msgbox(mock_print: str, input: str, expected: Any) -> None:
     ctx = table.definitions["hello"]
     interpreter.visitChildren(ctx)
     mock_print.assert_called_with(expected)
+
+def test_function_not_defined() -> None:
+    file_path = 'tests/files/test.bas'
+    try:
+        os.remove(file_path)
+    except FileNotFoundError:
+        # File did not exist; ignore the error
+        pass
+    with open(file_path, "w", newline='\r\n') as file:
+        file.write('Attribute VB_NAME = "HelloWorld"\n')
+        file.write('Function hello()\n')
+        file.write('    ' + MsgBox hello1() + '\n')
+        file.write('End Function\n')
+    input_stream = FileStream(file_path)
+    lexer = Lexer(input_stream)
+    ts = CommonTokenStream(lexer)
+    vbaparser = Parser(ts)
+    tree = vbaparser.module()
+    table = SymbolTable()
+    listener = VbaListener(table)
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    interpreter = VbaVisitor(table)
+    interpreter.visit(tree)
+    ctx = table.definitions["hello"]
+    with pytest.raises(VbaCompileException):
+        interpreter.visitChildren(ctx)
+    interpreter.visitChildren(ctx)
