ceb481bdae7292d1a11ac13d5d7f03c2fe0b3953
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-20 15:00:09 -0400

    Update test_interpreter.py

--- tests/Functional/test_interpreter.py
+++ tests/Functional/test_interpreter.py
@@ -75,6 +75,10 @@ def test_msgbox(mock_print: str, input: str, expected: Any) -> None:
     walker = ParseTreeWalker()
     walker.walk(listener, tree)
     interpreter = VbaVisitor(table)
+    table.definitions["msgbox"] = {
+        "type": "builtin",
+        "handle": "vba_stdlib.Interaction.MsgBox"
+    }
     ctx = table.definitions["hello"]
     interpreter.visit(ctx)
     mock_print.assert_called_with(expected)
