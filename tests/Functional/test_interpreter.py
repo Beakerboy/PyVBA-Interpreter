5ffd2a47d9a2254044a37c19706e1d058edd8169
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-21 08:14:32 -0400

    Update test_interpreter.py

--- tests/Functional/test_interpreter.py
+++ tests/Functional/test_interpreter.py
@@ -7,7 +7,9 @@ from antlr4_vba.vbaParser import vbaParser as Parser
 from pyvba_interpreter.symbol_table import SymbolTable
 from pyvba_interpreter.vba_listener import VbaListener
 from pyvba_interpreter.vba_visitor import VbaVisitor
-from pyvba_interpreter.Exceptions.vba_compile_exception import VbaCompileException
+from pyvba_interpreter.Exceptions.vba_compile_exception import (
+    VbaCompileException
+)
 from typing import Any
 from unittest.mock import patch
 
