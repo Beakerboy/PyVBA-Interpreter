2b851a027100ac47c532cf76a87ee0a9ebc9b995
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-21 14:36:45 -0400

    Update test_interpreter.py

--- tests/Functional/test_interpreter.py
+++ tests/Functional/test_interpreter.py
@@ -153,6 +153,38 @@ def test_override(mock_print: str) -> None:
     mock_print.assert_not_called()
 
 
+def test_missing_argument() -> None:
+    file_path = 'tests/files/test.bas'
+    try:
+        os.remove(file_path)
+    except FileNotFoundError:
+        # File did not exist; ignore the error
+        pass
+    with open(file_path, "w", newline='\r\n') as file:
+        file.write('Attribute VB_NAME = "HelloWorld"\n')
+        file.write('Function hello()\n')
+        file.write('    MsgBox\n')
+        file.write('End Function\n')
+    input_stream = FileStream(file_path)
+    lexer = Lexer(input_stream)
+    ts = CommonTokenStream(lexer)
+    vbaparser = Parser(ts)
+    tree = vbaparser.module()
+    table = SymbolTable()
+    listener = VbaListener(table)
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    interpreter = VbaVisitor(table)
+    table.library_definitions["msgbox"] = {
+        "type": "builtin",
+        "handle": getattr(Interaction, "MsgBox")
+    }
+    ctx = table.definitions["hello"]["handle"]
+    with pytest.raises(VbaCompileException) as e:
+        interpreter.visitChildren(ctx)
+    assert str(e.value) == "Argument not optional"
+
+
 def futuretest_use_sub_as_function() -> None:
     file_path = 'tests/files/test.bas'
     try:
