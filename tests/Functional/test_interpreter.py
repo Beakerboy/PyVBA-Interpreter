72db999239a03209b46f1193504c3054d38f0772
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-21 08:13:16 -0400

    Update test_interpreter.py

--- tests/Functional/test_interpreter.py
+++ tests/Functional/test_interpreter.py
@@ -113,3 +113,32 @@ def test_function_not_defined() -> None:
     ctx = table.definitions["hello"]["handle"]
     with pytest.raises(VbaCompileException):
         interpreter.visitChildren(ctx)
+
+
+def test_use_sub_as_function() -> None:
+    file_path = 'tests/files/test.bas'
+    try:
+        os.remove(file_path)
+    except FileNotFoundError:
+        # File did not exist; ignore the error
+        pass
+    with open(file_path, "w", newline='\r\n') as file:
+        file.write('Attribute VB_NAME = "HelloWorld"\n')
+        file.write('Function hello()\n')
+        file.write('    Foo = Hello1()\n')
+        file.write('End Function\n')
+        file.write('Sub Hello1()\n')
+        file.write('End Sub\n')
+    input_stream = FileStream(file_path)
+    lexer = Lexer(input_stream)
+    ts = CommonTokenStream(lexer)
+    vbaparser = Parser(ts)
+    tree = vbaparser.module()
+    table = SymbolTable()
+    listener = VbaListener(table)
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    interpreter = VbaVisitor(table)
+    ctx = table.definitions["hello"]["handle"]
+    with pytest.raises(VbaCompileException):
+        interpreter.visitChildren(ctx)
