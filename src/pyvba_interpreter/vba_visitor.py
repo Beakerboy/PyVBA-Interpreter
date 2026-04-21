6db99497f1f735e4f253759f9c8b719b08ebd80f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-21 10:33:49 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -130,3 +130,11 @@ class VbaVisitor(Visitor):
                 raise VbaCompileException("Sub or Function not defined")
             if self.table.definitions[name]["type"] == "sub":
                 raise VbaCompileException("Unexpected Function or variable")
+
+    def vistLExpression(                                        # noqa: N802
+            self: T,
+            ctx: Parser.LetStatementContext) -> Any:
+        if ctx.argumentList() is not None:
+            name = ctx.getChild(0).getText().lower()
+            if name not in self.table.definitions:
+                raise VbaCompileException("Sub or Function not defined")
