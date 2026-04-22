60cc1dd85ddb095fafdddf8a0c919e8d59be868d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-22 12:33:36 -0400

    Convert command to lowercase in execute_function

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -205,6 +205,7 @@ class VbaVisitor(Visitor):
 
     def execute_function(self: T, command: str,
                          args: list, no_sub: bool) -> Any:
+        command = command.lower()
         if (
             command not in self.table.definitions and
             command not in self.table.library_definitions
