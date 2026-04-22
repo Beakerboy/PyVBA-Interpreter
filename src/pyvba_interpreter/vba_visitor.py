c20507a8bc8d9ef69c9ab9776eae89c5e184eb8c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-22 11:59:53 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -219,7 +219,7 @@ class VbaVisitor(Visitor):
                 command: None
             }
             i = 0
-            for param in mod_def[params]:
+            for param in mod_def["params"]:
                 if not param["optional"]:
                     current_env[param["name"]] = args[i]
                     i += 1
