583611bb9fa31b38df69f99d1ae1636aebeb4bb8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-22 14:58:39 -0400

    Implement test_no_else for VB function execution (#9)
    * Add and test function execution without else clause in VB code.

--- tests/Functional/test_visitor.py
+++ tests/Functional/test_visitor.py
@@ -166,6 +166,36 @@ def test_factorial() -> None:
     assert result == expected
 
 
+def test_no_else() -> None:
+    file_path = 'tests/files/test.bas'
+    try:
+        os.remove(file_path)
+    except FileNotFoundError:
+        # File did not exist; ignore the error
+        pass
+    with open(file_path, "w", newline='\r\n') as file:
+        file.write('Attribute VB_NAME = "Factorial"\n')
+        file.write('Function Fact(num)\n')
+        file.write('    Fact = 1\n')
+        file.write('    If Num > 1 Then\n')
+        file.write('        Fact = Num * Fact(Num - 1)\n')
+        file.write('    End If\n')
+        file.write('End Function\n')
+    input_stream = FileStream(file_path)
+    lexer = Lexer(input_stream)
+    ts = CommonTokenStream(lexer)
+    vbaparser = Parser(ts)
+    tree = vbaparser.module()
+    table = SymbolTable()
+    listener = VbaListener(table)
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    interpreter = VbaVisitor(table)
+    result = interpreter.execute_function("fact", [5], True)
+    expected = 120
+    assert result == expected
+
+
 def test_function_not_defined() -> None:
     file_path = 'tests/files/test.bas'
     try:
