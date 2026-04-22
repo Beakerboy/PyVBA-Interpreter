7c6b2de42a700ecba00a08e3f25e1b64ba9ef545
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-22 10:44:23 -0400

    Remove test_interpreter from test_visitor.py
    Removed test_interpreter function and its implementation.

--- tests/Functional/test_visitor.py
+++ tests/Functional/test_visitor.py
@@ -15,20 +15,6 @@ from unittest.mock import patch
 
 
 @patch('builtins.print')
-def test_interpreter(mock_print: str) -> None:
-    input_stream = FileStream('tests/files/HelloWorld.bas')
-    lexer = Lexer(input_stream)
-    ts = CommonTokenStream(lexer)
-    vbaparser = Parser(ts)
-    tree = vbaparser.module()  # or module?
-    table = SymbolTable()
-    listener = VbaListener(table)
-    walker = ParseTreeWalker()
-    walker.walk(listener, tree)
-    assert len(table.definitions) == 1
-
-
-@patch('builtins.print')
 @pytest.mark.parametrize(
     "input, expected", [
         ('Call MsgBox("Hello World")', "Hello World"),
