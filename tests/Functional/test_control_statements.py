bdaf635a6ae057e99d05f4b0d09d4b568c529871
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-24 12:36:09 -0400

    Fix indentation and syntax in test_control_statements

--- tests/Functional/test_control_statements.py
+++ tests/Functional/test_control_statements.py
@@ -45,6 +45,10 @@ def build_interp(code: str) -> VbaVisitor:
          '        Fact = Num * Fact(Num - 1)\n'
          '    End If\n'
          'End Function\n'),
+        ('Function Fact(num)\n'
+         '    Fact = 1\n'
+         '    If Num > 1 Then Fact = Num * Fact(Num - 1)\n'
+         'End Function\n'),
         ('Function Fact(Num)\n'
          '    Fact = 1\n'
          '    While Num > 1\n'
