de370cfed77f188ecf4cb7545ae4c489e075c6a5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-24 12:35:25 -0400

    Update exit_for_exception.py

--- src/pyvba_interpreter/Exceptions/exit_for_exception.py
+++ src/pyvba_interpreter/Exceptions/exit_for_exception.py
@@ -1,2 +1,9 @@
-class ExitForException(Exception):
-    pass
+from .vba_compile_exception import VbaCompileException
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='ExitForException')
+
+
+class ExitForException(VbaCompileException):
+    super().__init__("Exit For not within For...Next")
