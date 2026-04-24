52c8d6d79e5b85dc9e2a2b39a416094004500986
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-24 06:59:53 -0400

    Clean up imports in test_control_statements.py
    Removed unused imports from the test_control_statements.py file.

--- tests/Functional/test_control_statements.py
+++ tests/Functional/test_control_statements.py
@@ -1,16 +1,10 @@
 import os
-import pytest
-from vba_stdlib.interaction import Interaction
 from antlr4 import CommonTokenStream, FileStream, ParseTreeWalker
 from antlr4_vba.vbaLexer import vbaLexer as Lexer
 from antlr4_vba.vbaParser import vbaParser as Parser
 from pyvba_interpreter.symbol_table import SymbolTable
 from pyvba_interpreter.vba_listener import VbaListener
 from pyvba_interpreter.vba_visitor import VbaVisitor
-from pyvba_interpreter.Exceptions.vba_compile_exception import (
-    VbaCompileException
-)
-from typing import Any
 from unittest.mock import patch
 
 
