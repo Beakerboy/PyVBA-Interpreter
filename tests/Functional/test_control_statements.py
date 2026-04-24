f63f5e6f0450768d53525e1f86eb9e5da58ea87d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-24 13:33:51 -0400

    Fix function signature for test_exit_sub_exception

--- tests/Functional/test_control_statements.py
+++ tests/Functional/test_control_statements.py
@@ -87,7 +87,7 @@ def test_factorial(code: str) -> None:
     assert result == expected
 
 
-def test_exit_sub_exception(code: str) -> None:
+def test_exit_sub_exception() -> None:
     code = ('Function Fact(Num)\n'
             '    Exit For\n'
             'End Function\n')
