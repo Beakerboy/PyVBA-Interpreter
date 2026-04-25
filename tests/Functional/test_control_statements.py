cf7fc0c5535ab0d7a143c94cb5cb6d19aaeaf396
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-25 16:39:31 -0400

    Update test_control_statements.py

--- tests/Functional/test_control_statements.py
+++ tests/Functional/test_control_statements.py
@@ -72,10 +72,23 @@ def test_factorial(code: str) -> None:
     assert result == expected
 
 
-def test_exit_sub_exception() -> None:
-    code = ('Function Fact(Num)\n'
-            '    Exit For\n'
-            'End Function\n')
+@pytest.mark.parametrize(
+    "code", [
+        ('Function Fact(Num)\n'
+         '    Exit For\n'
+         'End Function\n'),
+        ('Function Fact(Num)\n'
+         '    Fact = 1\n'
+         '    For I = 1 To Num + 2\n'
+         '        Fact = I * Fact\n'
+         '        If I = Num Then Bar\n'
+         '    Next I\n'),
+         'End Function\n
+         'Function Bar()\n
+         '    Exit For\n
+         'End Function\n'),
+        
+def test_exit_sub_exception(code: str) -> None:
     interpreter = build_interp(code)
     with pytest.raises(ExitForException):
         interpreter.execute_function("fact", [5], True)
