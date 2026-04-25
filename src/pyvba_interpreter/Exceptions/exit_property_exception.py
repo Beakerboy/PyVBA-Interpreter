9fe0a65b2f7b6b23a5b66be6893956e5a1713d7a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-25 16:35:13 -0400

    Create exit_property_exception.py

---
+++ src/pyvba_interpreter/Exceptions/exit_property_exception.py
@@ -0,0 +1,11 @@
+from .vba_compile_exception import VbaCompileException
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='ExitPropertyException')
+
+
+class ExitPropertyException(VbaCompileException):
+
+    def __init__(self: T) -> None:
+        super().__init__("Exit For not within For...Next")
