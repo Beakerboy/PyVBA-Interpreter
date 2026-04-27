568bd0695bbcbee84e0485a1af5cc8f42938644d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-27 07:47:40 -0400

    Fix message initialization in ExitDoException

--- src/pyvba_interpreter/Exceptions/exit_do_exception.py
+++ src/pyvba_interpreter/Exceptions/exit_do_exception.py
@@ -8,4 +8,5 @@ T = TypeVar('T', bound='ExitDoException')
 class ExitDoException(VbaCompileException):
 
     def __init__(self: T) -> None:
-        super().__init__("Exit Do not within Do...Loop")
+        self.msg = "Exit Do not within Do...Loop"
+        super().__init__(self.msg)
