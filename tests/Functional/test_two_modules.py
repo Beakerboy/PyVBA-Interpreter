3c3d8d866ed9488941c9387d33faaf5de5003748
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-27 11:55:28 -0400

    Implement test for two modules in VBA
    Add a test for two VBA modules interacting with each other.

---
+++ tests/Functional/test_two_modules.py
@@ -0,0 +1,51 @@
+import os
+import pytest
+from antlr4 import CommonTokenStream, FileStream, ParseTreeWalker
+from antlr4_vba.vbaLexer import vbaLexer as Lexer
+from antlr4_vba.vbaParser import vbaParser as Parser
+from pyvba_interpreter.symbol_table import SymbolTable
+from pyvba_interpreter.vba_listener import VbaListener
+from pyvba_interpreter.vba_visitor import VbaVisitor
+from pyvba_interpreter.Exceptions.vba_compile_exception import (
+    VbaCompileException
+)
+
+
+def build_interp(name: str, code: str, table: SymbolTable) -> VbaVisitor:
+    code = f'Attribute VB_NAME = "{name}"\n{code}'
+    file_path = f'tests/files/{name}.bas'
+    try:
+        os.remove(file_path)
+    except FileNotFoundError:
+        # File did not exist; ignore the error
+        pass
+    with open(file_path, "w", newline='\r\n') as file:
+        file.write(code)
+    input_stream = FileStream(file_path)
+    lexer = Lexer(input_stream)
+    ts = CommonTokenStream(lexer)
+    vbaparser = Parser(ts)
+    tree = vbaparser.module()
+    listener = VbaListener(table)
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    return table
+
+
+@pytest.mark.parametrize(
+    "code1, code2", [
+        ('Function Foo()\n'
+         '    Foo = Bar()\n'
+         'End Function\n',
+         'Function Bar()\n'
+         '    Bar = 42\n'
+         'End Function\n'),        
+    ])
+def test_two_files(code1: str, code2: str) -> None:
+    table = SymbolTable()
+    build_interp("FooModule", code1, table)
+    build_interp("BarModule", code2, table)
+    visitor = VbaVisitor(table)
+    result = interpreter.execute_function("foo", [], True)
+    expected = 42
+    assert result == expected
