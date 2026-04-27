0c4a28a2e71a36a2a663797a54756b1f98d70cf3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-27 08:09:43 -0400

    Update vba_compile_exception.py

--- src/pyvba_interpreter/Exceptions/vba_compile_exception.py
+++ src/pyvba_interpreter/Exceptions/vba_compile_exception.py
@@ -10,3 +10,6 @@ class VbaCompileException(VbaException):
     def __init__(self: T, msg: str) -> None:
         self.msg = msg
         super().__init__("Compile error:\n" + msg)
+
+    def __str__(self: T) -> str:
+        return "Compile error:\n" + self.msg
