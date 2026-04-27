4089f6de63cc2a3cb521aa24d32abaa56aa2c99b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-26 21:50:23 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -337,11 +337,11 @@ class VbaVisitor(Visitor):
                 except ExitForException as e:
                     self.raise_for_except = True
                     raise e
-                except ExitFunctionException as e
+                except ExitFunctionException as e:
                     if mod_def["type"] == "sub" or self.raise_function_except
                         self.raise_function_except = True
                         raise e
-                except ExitSubException as e
+                except ExitSubException as e:
                     if mod_def["type"] == "function" or self.raise_sub_except
                         self.raise_sub_except = True
                         raise e
