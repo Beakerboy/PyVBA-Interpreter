4181121c5d828e228a2a78bb7622b6e3b503cd39
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-28 12:06:07 -0400

    Update test_two_modules.py

--- tests/Functional/test_two_modules.py
+++ tests/Functional/test_two_modules.py
@@ -64,3 +64,20 @@ def test_two_files(code1: str, code2: str) -> None:
     result = visitor.execute_function("foo", [])
     expected = 42
     assert result == expected
+
+
+@pytest.mark.parametrize(
+    "code1, code2", [
+        ('Function Foo()\n'
+         '    Foo = BarModule.Baz()\n'
+         'End Function\n',
+         ''),
+    ])
+def test_two_files(code1: str, code2: str) -> None:
+    table = SymbolTable()
+    build_interp("FooModule", code1, table)
+    build_interp("BarModule", code2, table)
+    visitor = VbaVisitor(table)
+    with pytest.raises(VbaCompileException) as e
+        visitor.execute_function("foo", [])
+    assert str(e.value) == "Compile Error:\nMethod or data member not found"
