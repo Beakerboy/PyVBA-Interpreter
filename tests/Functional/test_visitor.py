d579abc6b249282f31ac4b2a6e98f892c1e00050
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-29 10:52:28 -0400

    Update test_visitor.py

--- tests/Functional/test_visitor.py
+++ tests/Functional/test_visitor.py
@@ -77,7 +77,9 @@ def test_msgbox(mock_print: str, input: str, expected: Any) -> None:
                 "type": FunctionType.FUNCTION,
                 "handle": getattr(Interaction, "MsgBox"),
                 "module": "vba"
-    }}}
+            }
+        }
+    }
     interpreter.execute_function("hello", [])
     mock_print.assert_called_with(expected)
 
