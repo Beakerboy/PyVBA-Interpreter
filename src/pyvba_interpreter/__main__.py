60ae7376f54fe99d1474eb61083c0ef46a333e37
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-29 10:22:42 -0400

    Update __main__.py

--- src/pyvba_interpreter/__main__.py
+++ src/pyvba_interpreter/__main__.py
@@ -32,8 +32,8 @@ def main() -> None:
 
     interpreter = VbaVisitor(table)
     interpreter.visit(tree)
-    if function_to_run in table.definitions:
-        target_node = table.definitions[function_to_run]
+    if function_to_run in table.definitions[module]["functions"]:
+        target_node = table.definitions[module]["functions"][function_to_run]
         interpreter.visitChildren(target_node)
     else:
         print("error:")
