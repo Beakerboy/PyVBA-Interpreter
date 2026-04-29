f33207c6252392f8b7275b15f6dcb4f9cc803852
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-29 10:46:03 -0400

    Update vba_listener.py

--- src/pyvba_interpreter/vba_listener.py
+++ src/pyvba_interpreter/vba_listener.py
@@ -48,12 +48,12 @@ class VbaListener(Listener):
             self: T,
             ctx: Parser.SubroutineDeclarationContext) -> None:
         name = ctx.subroutineName().getText()
-        if name.lower() in self.table.definitions[self.module_name.lower()]:
+        funcs = self.table.definitions[self.module_name.lower()]["functions"]
+        if name.lower() in funcs:
             raise VbaCompileException(f"Ambiguous name detected: {name}")
         # Save the context (subtree) so the Visitor can find it later
         params = self._get_params(ctx.procedureParameters())
-        self.table.definitions[self.module_name.lower()][
-                "functions"][name.lower()] = {
+        funcs[name.lower()] = {
             "type": FunctionType.SUB,
             "module": self.module_name.lower(),
             "handle": ctx.procedureBody(),
