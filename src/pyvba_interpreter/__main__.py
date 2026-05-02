172f4db7697426130d72ce84d9a363e1d3766722
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-02 13:41:56 -0400

    Update __main__.py

--- src/pyvba_interpreter/__main__.py
+++ src/pyvba_interpreter/__main__.py
@@ -28,7 +28,7 @@ def main() -> None:
     vbaparser = Parser(ts)
     tree = vbaparser.module()
     table = SymbolTable()
-    listener = VbaListener(table)
+    listener = VbaListener(project, table)
     walker = ParseTreeWalker()
     walker.walk(listener, tree)
 
