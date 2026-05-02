54096235751f70a2c742a5171a12410115b805e7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-02 15:47:54 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -514,8 +514,9 @@ class VbaVisitor(Visitor):
     def _find_function_in_definition(
             self: T, command: str, cur_module: str
     ) -> FunctionDefinition | LibraryDefinition:
-        if cur_module != "" and command in self.table.definitions[cur_module]:
-            return self.table.definitions[cur_module]["functions"][command]
+        for proj in self.table.definitions.values(): 
+            if cur_module != "" and command in proj["modules"][cur_module]:
+                return self.table.definitions[cur_module]["functions"][command]
         for key, proj in self.table.definitions.items():
             if key == command:
                 msg = "Expected variable or procedure, not project"
