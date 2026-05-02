2e4eb139cb9bf8332e968efe1b369bd314854ec8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-02 18:20:06 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -3,8 +3,7 @@ from antlr4_vba.vbaParser import ParserRuleContext, vbaParser as Parser
 from antlr4_vba.vbaParserVisitor import vbaParserVisitor as Visitor
 from vba_stdlib.literal_factory import literal_from_string
 from .symbol_table import (
-    FunctionDefinition, FunctionType, LibModuleDefinition, LibraryDefinition,
-    ModuleDefinition, SymbolTable
+    FunctionDefinition, FunctionType, LibraryDefinition, SymbolTable
 )
 from .Exceptions.vba_compile_exception import VbaCompileException
 from .Exceptions.exit_do_exception import ExitDoException
