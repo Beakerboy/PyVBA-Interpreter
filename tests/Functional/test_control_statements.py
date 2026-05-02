d0ea8db66eeabc187a79bed9e58fabcfdd61ccf2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-02 19:26:05 -0400

    Update test_control_statements.py

--- tests/Functional/test_control_statements.py
+++ tests/Functional/test_control_statements.py
@@ -27,7 +27,7 @@ def build_interp(code: str) -> VbaVisitor:
     vbaparser = Parser(ts)
     tree = vbaparser.module()
     table = SymbolTable()
-    listener = VbaListener(table)
+    listener = VbaListener("vbaproject", table)
     walker = ParseTreeWalker()
     walker.walk(listener, tree)
     return VbaVisitor(table)
