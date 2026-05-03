6dc97d22c6d2ec16eef5384bc2bc08c4dd912151
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-02 20:04:10 -0400

    Fix function retrieval in test_visitor.py

--- tests/Functional/test_visitor.py
+++ tests/Functional/test_visitor.py
@@ -78,7 +78,8 @@ def test_msgbox(mock_print: str, input: str, expected: Any) -> None:
             }
         }
     }
-    func = table["vbaproject"]["modules"]["helloworld"]["functions"]["hello"]
+    modules = interpreter.table.library_definitions["vbaproject"]["modules"]
+    func = modules["helloworld"["functions"]["hello"]
     interpreter.run_function(func, [])
     mock_print.assert_called_with(expected)
 
