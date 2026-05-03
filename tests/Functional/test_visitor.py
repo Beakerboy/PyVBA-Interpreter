a1969e7621b817b20643680a2fbe21242a8aa067
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-02 20:24:30 -0400

    Update test_visitor.py

--- tests/Functional/test_visitor.py
+++ tests/Functional/test_visitor.py
@@ -128,7 +128,7 @@ def test_function_arguments(
             '    ' + input + '\n'
             'End Function\n')
     visitor = build_interp(code)
-    modules = interpreter.table.library_definitions["vbaproject"]["modules"]
+    modules = visitor.table.library_definitions["vbaproject"]["modules"]
     func = modules["helloworld"]["functions"]["hello"]
     result = interpreter.run_function(func, [])
     assert result == expected
@@ -213,9 +213,9 @@ def test_two_functions() -> None:
             '    Bar = 2\n'
             'End Function\n')
     visitor = build_interp(code)
-    modules = interpreter.table.library_definitions["vbaproject"]["modules"]
+    modules = visitor.table.library_definitions["vbaproject"]["modules"]
     func = modules["helloworld"]["functions"]["foo"]
-    result = interpreter.run_function(func, [])
+    result = visitor.run_function(func, [])
     assert result == 2
 
 
