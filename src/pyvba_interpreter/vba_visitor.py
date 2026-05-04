e594a9591b24c56bc8856c9ce215d0dd53fe15b5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-04 12:45:15 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -449,7 +449,7 @@ class VbaVisitor(Visitor):
                 raise VbaCompileException("Argument not optional")
             if len(args) > max:
                 msg = ("Wrong number of arguments or invalid property"
-                    "assignment")
+                       "assignment")
                 raise VbaCompileException(msg)
             i = 0
             for param in defn["params"]:
@@ -502,7 +502,7 @@ class VbaVisitor(Visitor):
                 raise VbaCompileException("Argument not optional")
             if len(args) > max:
                 msg = ("Wrong number of arguments or invalid property"
-                    "assignment")
+                       "assignment")
                 raise VbaCompileException(msg)
             output = ctx(*args)
         else:
