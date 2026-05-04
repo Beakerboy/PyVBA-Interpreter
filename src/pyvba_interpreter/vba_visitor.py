4412f3c86563a41091133e73d6b537cb7c28d0e3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-04 08:21:51 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -422,6 +422,9 @@ class VbaVisitor(Visitor):
             return self.table.definitions[name]
         if name in self.table.library_definitions:
             return self.table.library_definitions[name]
+        for proj in self.table.definitions:
+            if name in proj["modules"]:
+                return proj["modules"][name]
         raise VbaCompileException("Method or data member not found")
 
     def run_function(self: T,
