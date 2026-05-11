2a9b74ffc6a77d5faaa3ce294cdbaa3920031bbe
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-11 13:04:14 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -409,7 +409,7 @@ class VbaVisitor(Visitor):
         if isinstance(defn, Callable):
             return VBAArray(*args)
         if isinstance(defn, VBAArray):
-            return defn[int(args[0]))
+            return defn[int(args[0])]
         return self.run_function(defn, args)
 
     # Only used within implicit call statement.
