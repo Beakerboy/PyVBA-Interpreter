3e8f4961a8f2e2e4b5e2e2981c6b9d7bba01c98a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-12 17:20:50 -0400

    Update vba_visitor.py (#42)

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -78,7 +78,7 @@ class VbaVisitor(Visitor):
                 if defn["type"] == "sub":
                     raise VbaCompileException("Expected Function or variable")
                 else:
-                    raise VbaException(f"Error On Line: {ctx.start.line}")
+                    raise VbaException(f"Error On Line {ctx.start.line}, {var_name}")
         value = self.visit(ctx.expression())
         if isinstance(value, tuple):
             value = value[1]
