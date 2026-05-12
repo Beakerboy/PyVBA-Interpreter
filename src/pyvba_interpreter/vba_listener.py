a5fdc9cb82319ee994e24d3d639596532f6e01c4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-12 09:41:25 -0400

    Refactor parameter name extraction in vba_listener.py

--- src/pyvba_interpreter/vba_listener.py
+++ src/pyvba_interpreter/vba_listener.py
@@ -90,7 +90,8 @@ class VbaListener(Listener):
             if parameter_list.positionalParameters() is not None:
                 pos_params = parameter_list.positionalParameters()
                 while pos_params.positionalParam(i) is not None:
-                    name = pos_params.positionalParam(i).getText().lower()
+                    untyped_name = pos_params.positionalParam(i).paramDcl().untypedNameParamDcl()
+                    name = untyped_name.ambiguousIdentifier().getText().lower()
                     param: ParamDefinition = {
                         "name": name,
                         "optional": False,
