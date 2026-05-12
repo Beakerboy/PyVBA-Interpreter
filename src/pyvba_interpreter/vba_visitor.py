8554ba9725aab80a75e61e6e22965e56c6ca30f1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-12 08:43:54 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -607,6 +607,6 @@ class VbaVisitor(Visitor):
     @staticmethod
     def _new_type_from_string(type_name: str) -> Any:
         if type_name == "integer":
-            return vba_types.integer.VBAInteger()
+            return vba_types.VBAInteger()
         if type_name == "variant":
-            return vba_types.integer.VBAEmpty()
+            return vba_types.VBAEmpty()
