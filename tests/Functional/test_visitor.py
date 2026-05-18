8b028441d3a75a20641d04238f8ae620f11056b3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-18 08:57:07 -0400

    Update test_visitor.py (#51)
    * Update test_visitor.py
    
    * Update vba_visitor.py
    
    * Add test case for string concatenation with integers
    
    * Update vba_visitor.py

--- tests/Functional/test_visitor.py
+++ tests/Functional/test_visitor.py
@@ -105,6 +105,8 @@ def test_msgbox(input: str, expected: Any, mocker: MockerFixture) -> None:
         ('hello = 10 Mod 3', 1),
         ('hello = 10 \\ 3', 3),
         ('hello = 2 ^ 2', 4),
+        ('hello = "Hello" & " World"', "Hello World"),
+        ('hello = 1 & 2 & "Hello"', "12Hello"),
         ('hello = 1\n'
          'Exit Function\n'
          'hello = 2\n', 1),
