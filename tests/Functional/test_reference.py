7ac090840d168ca0c423d31887a1176dfc130454
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-21 13:38:16 -0400

    Update test_reference.py

--- tests/Functional/test_reference.py
+++ tests/Functional/test_reference.py
@@ -38,9 +38,9 @@ def build_interp(code: str) -> VbaVisitor:
          '    Bar Num\n'
          '    Foo = Num\n'
          'End Function\n'
-         'Function Bar(Num1)\n'
+         'Sub Bar(Num1)\n'
          '    Num1 = 11\n'
-         'End Function\n', 11),
+         'End Sub\n', 11),
         ('Function Foo()\n'
          '    Num = 10\n'
          '    Bar Num\n'
@@ -54,7 +54,7 @@ def test_byref(code: str, expected: int) -> None:
     visitor = build_interp(code)
     modules = visitor.table.definitions["vbaproject"]["modules"]
     func = modules["factorial"]["functions"]["foo"]
-    result = visitor.run_function(func, [VBAInteger(5)])
+    result = visitor.run_function(func, [])
     expected = 120
     assert isinstance(result, VBAInteger)
     assert int(result) == expected
