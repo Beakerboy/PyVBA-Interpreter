423b92ed4537fd26a385c31d0f8c0334ca408dd6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-21 14:17:54 -0400

    Update test_reference.py

--- tests/Functional/test_reference.py
+++ tests/Functional/test_reference.py
@@ -6,6 +6,9 @@ from antlr4_vba.vbaParser import vbaParser as Parser
 from pyvba_interpreter.symbol_table import SymbolTable
 from pyvba_interpreter.vba_listener import VbaListener
 from pyvba_interpreter.vba_visitor import VbaVisitor
+from pyvba_interpreter.Exceptions.vba_compile_exception import (
+    VbaCompileException
+)
 from vba_types import VBAInteger
 
 
