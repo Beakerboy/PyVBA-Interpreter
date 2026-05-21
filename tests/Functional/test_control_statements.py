770647b026c159e4a644d4d48807663a0589964c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-21 07:02:11 -0400

    Update test_control_statements.py

--- tests/Functional/test_control_statements.py
+++ tests/Functional/test_control_statements.py
@@ -75,8 +75,8 @@ def build_interp(code: str) -> VbaVisitor:
          '        Fact = Fact * Num\n'
          '        Num = Num - 1\n'
          '    Loop While Num > 1\n'),
-        ('    If Num = 3 Then\n'
-         '        Fact = 6\n'
+        ('    If Num = 4 Then\n'
+         '        Fact = 24\n'
          '    Else\n'
          '        Fact = Num * Fact(Num - 1)\n'
          '    End If\n'),
