b753211f41f968fe1441726f3fc53a31ecad3a04
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-22 14:37:07 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -161,14 +161,16 @@ class VbaVisitor(Visitor):
                 self.visit(ctx.statementBlock())
         else:
             i = 0
+            run_else = False
             while ctx.elseIfBlock(i) is not None:
                 condition = self.visit(ctx.elseIfBlock(i).booleanExpression())
                 if bool(condition):
                     if ctx.elseIfBlock(i).statementBlock() is not None:
                         self.visit(ctx.elseIfBlock(i).statementBlock())
+                    run_else = True
                     break
                 i += 1
-            if ctx.elseBlock() is not None:
+            if not run_else and ctx.elseBlock() is not None:
                 self.visit(ctx.elseBlock().statementBlock())
 
     def visitSingleLineIfStatement(                                # noqa: N802
