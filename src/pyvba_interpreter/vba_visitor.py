a0ec4ff40e4d3cd542f5f0271c71bae5274ece18
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-06-22 09:54:19 -0400

    Update vba_visitor.py

--- src/pyvba_interpreter/vba_visitor.py
+++ src/pyvba_interpreter/vba_visitor.py
@@ -106,10 +106,13 @@ class VbaVisitor(Visitor):
             else:
                 raise Exception("Unsupported")
         else:
-            #try:
-            command = self.visit(first_child)
-            #except VbaCompileException:
-            #raise VbaCompileException("Sub or Function not defined")
+            try:
+                command = self.visit(first_child)
+            except VbaCompileException as e:
+                err_msg = "Method or data member not found "
+                if str(e.value)[:47] == "Compile error:\n" + err_msg
+                    raise VbaCompileException("Sub or Function not defined")
+                raise e
             args = []
             if ctx.argumentList() is not None:
                 args = self.visit(ctx.argumentList())
